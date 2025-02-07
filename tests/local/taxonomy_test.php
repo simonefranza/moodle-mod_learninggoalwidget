eb0a85b432f64b88f18fc69e172ab1e0ce770a84
Simone Franza <simonefranza3@gmail.com>
2025-02-07 19:08:19 +0100

    Taxonomy tests

--- tests/local/taxonomy_test.php
+++ tests/local/taxonomy_test.php
@@ -109,5 +109,78 @@ final class taxonomy_test extends \advanced_testcase {
         $this->assertSame($taxonomy->name, "name");
         $this->assertTrue(isset($taxonomy->children) && is_array($taxonomy->children));
         $this->assertTrue(count($taxonomy->children) == 0);
+        $numtopics = 50;
+        $numgoals = 10;
+        $topics = [];
+        // Create 50 topics with 10 goals each
+        for ($i = 0; $i < $numtopics; $i++) {
+            $goals = [];
+            for ($ii = 0; $ii < $numgoals; $ii++) {
+                $newgoal= (object) [
+                    'name' => 'T' . $i . 'G' . $ii,
+                    'keyword' => 'T' . $i . 'G' . $ii,
+                    'link' => 'http://topic' . $i . 'goal' . $ii . '.com',
+                    'ranking' => $ii + 1,
+                    'goalid' => $i * $numtopics + $ii,
+                    'new' => true,
+                ];
+                $goals[] = $newgoal;
+            }
+            $newtopic = (object) [
+                'name' => 'T' . $i,
+                'keyword' => 'T' $i,
+                'link' => 'http://topic' . $i . '.com',
+                'ranking' => $i + 1,
+                'topicid' => $i,
+                'children' => $goals,
+                'new' => true,
+            ];
+            $topics[] = $newtopic;
+        }
+
+        foreach ($topics as &$topic) {
+            $this->assertTrue(topic::validate_topic($topic));
+            $topic->topicid = topic::update_topic($lgwid, $topic);
+            $this->assertTrue($topic->topicid > 0);
+            foreach ($topic->children as &$goal) {
+                $this->assertTrue(goal::validate_goal($goal));
+                $goal->goalid = goal::update_goal($lgwid, $topic->topicid, $goal);
+                $this->assertTrue($goal->goalid > 0);
+            }
+        }
+
+        // Check that everything was added correctly
+        $taxonomy = json_decode(taxonomy::get_taxonomy_as_json($lgwid));
+        $this->assertNotNull($taxonomy);
+        $this->assertTrue(isset($taxonomy->name) && is_string($taxonomy->name));
+        $this->assertSame($taxonomy->name, "name");
+        $this->assertTrue(isset($taxonomy->children) && is_array($taxonomy->children));
+        $this->assertTrue(count($taxonomy->children) == $numtopics);
+        for ($i = 0; $i < $numtopics; $i++) {
+            $topic = $taxonomy->children[$i];
+            $this->assertTrue(isset($topic->name) && is_string($topic->name));
+            $this->assertSame($topic->name, 'T' . $i);
+            $this->assertTrue(isset($topic->shortname) && is_string($topic->shortname));
+            $this->assertSame($topic->shortname, 'T' . $i);
+            $this->assertTrue(isset($topic->link) && is_string($topic->link));
+            $this->assertSame($topic->link, 'http://topic' . $i . '.com');
+            $this->assertTrue(isset($topic->ranking) && is_int($topic->ranking));
+            $this->assertSame($topic->ranking, $i + 1);
+            $this->assertTrue(isset($topic->topicid) && is_int($topic->topicid));
+            $this->assertTrue(isset($topic->children) && is_array($topic->children));
+            $this->assertTrue(count($topic->children) == $numgoals);
+            for ($ii = 0; $ii < $numgoals; $ii++) {
+                $goal = $topic->children[$ii];
+                $this->assertTrue(isset($goal->name) && is_string($goal->name));
+                $this->assertSame($goal->name, 'T' . $i . 'G' . $ii);
+                $this->assertTrue(isset($goal->shortname) && is_string($goal->shortname));
+                $this->assertSame($goal->shortname, 'T' . $i . 'G' . $ii);
+                $this->assertTrue(isset($goal->link) && is_string($goal->link));
+                $this->assertSame($goal->link, 'http://topic' . $i . 'goal' . $ii . '.com');
+                $this->assertTrue(isset($goal->ranking) && is_int($goal->ranking));
+                $this->assertSame($goal->ranking, $ii + 1);
+                $this->assertTrue(isset($goal->goalid) && is_int($topic->goalid));
+            }
+        }
     }
 }
