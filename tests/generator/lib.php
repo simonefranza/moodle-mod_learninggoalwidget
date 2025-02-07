983c1cb953d50cdbe2167bbb15866ee7bea19952
Simone Franza <simonefranza3@gmail.com>
2025-02-07 09:37:32 +0100

    Tests

--- tests/generator/lib.php
+++ tests/generator/lib.php
@@ -65,7 +65,7 @@ class mod_learninggoalwidget_generator extends testing_module_generator {
             'maxattempts' => -1,
             'markingworkflow' => 0,
             'markingallocation' => 0,
-            'taxonomy' => $taxonomy,
+            'taxonomy' => json_encode($taxonomy, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK),
         ];
 
         foreach ($defaultsettings as $name => $value) {
