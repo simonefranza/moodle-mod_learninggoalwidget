db0c29b6630f3e7ff0138e577f4428af865ee6e9
Simone Franza <simonefranza3@gmail.com>
2025-02-09 19:51:01 +0100

    Widget test

---
+++ tests/output/widget/widget_test.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Learning Goal Taxonomy - Taxonomy Test
+ *
+ * @package   mod_learninggoalwidget
+ * @copyright 2025 Know Center GmbH
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_learninggoalwidget\output\widget;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/mod/learninggoalwidget/tests/utils.php');
+
+use mod_learninggoalwidget\output\widget\renderer;
+use mod_learninggoalwidget\output\widget\widget_renderable;
+
+/**
+ * Learning Goal Taxonomy Widget Renderable Test
+ *
+ * @package   mod_learninggoalwidget
+ * @copyright 2025 Know Center GmbH
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @runTestsInSeparateProcesses
+ */
+final class widget_renderable_test extends \advanced_testcase {
+    use \mod_learninggoalwidget\utils;
+    /**
+     * Test rendering a widget.
+     *
+     * @covers \mod_learninggoalwidget\output\widget\renderer
+     * @covers \mod_learninggoalwidget\output\widget\widget_renderable::__construct
+     * @covers \mod_learninggoalwidget\output\widget\widget_renderable::export_for_template
+     */
+    public function test_render_widget() {
+        $this->setup_widget();
+        $renderer = $this->get_renderer();
+        $output = $renderer->render_widget(); // Call the function that renders
+
+        $this->assertIsString($output);
+        $this->assertNotEmpty($output);
+        $this->assertStringContainsString('<div', $output); // Example check for HTML structure
+        $cm = get_coursemodule_from_id('learninggoalwidget', $res->instance->id);
+        $widget = new widget_renderable(
+            $res->course->id,
+            $res->user->id,
+            $cm->id,
+            $res->instance->id
+        );
+        $widgt->export_for_template($output);
+    }
+
+    /**
+     * Helper function to get the renderer.
+     */
+    private function get_renderer() {
+        global $PAGE;
+        return $PAGE->get_renderer('mod_learninggoalwidget', 'widget');
+    }
+}
